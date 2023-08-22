44b918a2fb7cf07241f764f0463aa87855df18c8
fastreds <142920045+fastreds@users.noreply.github.com>
2023-08-22 09:50:56 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<table style="border-collapse: collapse; width: 39.2034%; height: 222px;" border="1">
+<tbody>
+<tr>
+<td style="width: 43.6171%;">Congreso tecnolog&iacute;as en Salud 2023</td>
+<td style="width: 12.0433%;"><img src="https://chart.googleapis.com/chart?chs=200x200&amp;cht=qr&amp;chl=204020472" alt="" width="200" height="200" /></td>
+</tr>
+</tbody>
+</table>
+</body>
+</html>
